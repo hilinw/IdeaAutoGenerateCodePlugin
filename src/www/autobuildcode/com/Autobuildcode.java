8c2a302b43c3ae9e7209d6f9232a3961e2e6502d
hilinw <441089@qq.com>
2020-05-25 12:12:46 +0800

    frist time

--- src/www/autobuildcode/com/Autobuildcode.java
+++ src/www/autobuildcode/com/Autobuildcode.java
@@ -7,27 +7,29 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiUtilBase;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 
 public class Autobuildcode extends AnAction {
 
     BuildCodeUI buildCodeUI = null;
+
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
 
-        if(buildCodeUI == null){
+        if (buildCodeUI == null) {
             buildCodeUI = new BuildCodeUI();
             buildCodeUI.setSize(600, 100);
 
             Toolkit kit = Toolkit.getDefaultToolkit();    // 定义工具包
             Dimension screenSize = kit.getScreenSize();   // 获取屏幕的尺寸
-            int screenWidth = screenSize.width/2;         // 获取屏幕的宽
-            int screenHeight = screenSize.height/2;       // 获取屏幕的高
+            int screenWidth = screenSize.width / 2;         // 获取屏幕的宽
+            int screenHeight = screenSize.height / 2;       // 获取屏幕的高
             int height = buildCodeUI.getHeight();
             int width = buildCodeUI.getWidth();
 
-            buildCodeUI.setLocation(screenWidth-width/2, screenHeight-height/2);
+            buildCodeUI.setLocation(screenWidth - width / 2, screenHeight - height / 2);
         }
 
         Project project = anActionEvent.getData(PlatformDataKeys.PROJECT);
@@ -36,11 +38,11 @@ public class Autobuildcode extends AnAction {
 
         String currentFile = "";
 
-        String projectPath  = project.getBasePath();
-        if(editor == null){
+        String projectPath = project.getBasePath();
+        if (editor == null) {
 
             currentFile = projectPath;
-        }else{
+        } else {
             PsiFile currentEditorFile = PsiUtilBase.getPsiFileInEditor(editor, project);
             currentFile = currentEditorFile.getContainingDirectory().getVirtualFile().getCanonicalPath();
 
@@ -51,11 +53,26 @@ public class Autobuildcode extends AnAction {
         buildCodeUI.getCurrentFile().setText(currentFile);
         //buildCodeUI.getCurrentFile().setName(currentFile);
 
+
         buildCodeUI.pack();
 
         buildCodeUI.setVisible(true);
 
 
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+
+        //TODO
+        if (false) {
+            //e.getPresentation().setEnabledAndVisible(false);
+            e.getPresentation().setEnabled(false);
+        } else {
+            //e.getPresentation().setEnabledAndVisible(true);
+            e.getPresentation().setEnabled(true);
+        }
 
     }
 }
